ad86cf2df0914ccc8d8a629ea4b1bd232ca115aa
Biasio <alessandro.biasioli@gmail.com>
2026-04-01 14:30:43 +0200

    removed old project defines

--- drivers/vl53l0x.h
+++ drivers/vl53l0x.h
@@ -21,21 +21,6 @@
 
 #define VL53L0X_OUT_OF_RANGE (8190)
 
-/* Comment these out if not connected */
-#define VL53L0X_SECOND
-#define VL53L0X_THIRD
-
-typedef enum
-{
-    VL53L0X_IDX_FIRST,
-#ifdef VL53L0X_SECOND
-    VL53L0X_IDX_SECOND,
-#endif
-#ifdef VL53L0X_THIRD
-    VL53L0X_IDX_THIRD,
-#endif
-} vl53l0x_idx_t;
-
 /**
  * Initializes the sensors in the vl53l0x_idx_t enum.
  * @note Each sensor must have its XSHUT pin connected.
