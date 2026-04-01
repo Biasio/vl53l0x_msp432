087b31cd9081e84017db83c245e67195e7e7c012
Biasio <alessandro.biasioli@gmail.com>
2026-04-01 18:19:35 +0200

    changed some macro expansion definitions

--- drivers/i2c.c
+++ drivers/i2c.c
@@ -2,52 +2,52 @@
 
 static bool start_transfer(uint16_t addr, uint8_t addr_len)
 {
-    USCI_REG_PTR(EUSCI_SEL,CTL1) |= UCTXSTT + UCTR; /* Set up master as TX and send start condition */
+    EUSCI_SEL->CTLW0 |= UCTXSTT + UCTR; /* Set up master as TX and send start condition */
 
     /* Send MSB first if 16-bit address */
     if (addr_len == 2) {
-        USCI_REG_VAR(EUSCI_SEL,TXBUF) = (addr >> 8) & 0xFF; //MSB
-        while (USCI_REG_PTR(EUSCI_SEL,CTL1) & UCTXSTT); /* Wait for start condition to be sent */
+        EUSCI_SEL->TXBUF = (addr >> 8) & 0xFF; //MSB
+        while (EUSCI_SEL->CTLW0 & UCTXSTT); /* Wait for start condition to be sent */
         /*If the slave does not acknowledge the transmitted data, the not-acknowledge interrupt flag UCNACKIFG is set*/
-        if (USCI_REG_VAR(EUSCI_SEL,STAT) & UCNACKIFG) return false;
-        while (!(IFG2 & USCI_REG_VAR(EUSCI_SEL,TXIFG))); /* Wait for byte to be sent */
+        if (EUSCI_SEL->IFG & EUSCI_B_IFG_NACKIFG) return false;
+        while (!(EUSCI_SEL->IFG & EUSCI_B_IFG_TXIFG0)); /* Wait for byte to be sent */
         /*If the slave does not acknowledge the transmitted data, the not-acknowledge interrupt flag UCNACKIFG is set*/
-        if (USCI_REG_VAR(EUSCI_SEL,STAT) & UCNACKIFG) return false;
+        if (EUSCI_SEL->IFG & EUSCI_B_IFG_NACKIFG) return false;
     }
 
     /* Send LSB (or 8-bit address) */
-    USCI_REG_VAR(EUSCI_SEL,TXBUF) = addr & 0xFF;
+    EUSCI_SEL->TXBUF = addr & 0xFF;
     if (addr_len == 1) {
-        while (USCI_REG_PTR(EUSCI_SEL,CTL1) & UCTXSTT);
+        while (EUSCI_SEL->CTLW0 & UCTXSTT);
     }
     /*If the slave does not acknowledge the transmitted data, the not-acknowledge interrupt flag UCNACKIFG is set*/
-    if (USCI_REG_VAR(EUSCI_SEL,STAT) & UCNACKIFG) return false;
+    if (EUSCI_SEL->IFG & EUSCI_B_IFG_NACKIFG) return false;
 
-    while (!(IFG2 & USCI_REG_VAR(EUSCI_SEL,TXIFG)));
+    while (!(EUSCI_SEL->IFG & EUSCI_B_IFG_TXIFG0));
     /*If the slave does not acknowledge the transmitted data, the not-acknowledge interrupt flag UCNACKIFG is set*/
-    return !(USCI_REG_VAR(EUSCI_SEL,STAT) & UCNACKIFG);
+    return !(EUSCI_SEL->IFG & EUSCI_B_IFG_NACKIFG);
 }
 
 static void stop_transfer()
 {
-    USCI_REG_PTR(EUSCI_SEL,CTL1) |= UCTXSTP; //Setting UCTXSTP generates a STOP condition after the next acknowledge from the slave 
-    while (USCI_REG_PTR(EUSCI_SEL,CTL1) & UCTXSTP); // wait for the end of the communication
+    EUSCI_SEL->CTLW0 |= UCTXSTP; //Setting UCTXSTP generates a STOP condition after the next acknowledge from the slave 
+    while (EUSCI_SEL->CTLW0 & UCTXSTP); // wait for the end of the communication
 }
 
 bool i2c_read_core(const uint16_t addr, uint8_t addr_len, uint8_t *data, uint16_t data_len)
 {
     if (!start_transfer(addr, addr_len)) return false; // start the transfer to request data
 
-    USCI_REG_PTR(EUSCI_SEL,CTL1) &= ~UCTR;   /* Configure as receiver */
-    USCI_REG_PTR(EUSCI_SEL,CTL1) |= UCTXSTT; /* Send RESTART condition */
-    while (USCI_REG_PTR(EUSCI_SEL,CTL1) & UCTXSTT); /* Wait for start condition to be received */
-    if (USCI_REG_VAR(EUSCI_SEL,STAT) & UCNACKIFG) return false; /*If the slave does not acknowledge the transmitted data, the not-acknowledge interrupt flag UCNACKIFG is set*/
+    EUSCI_SEL->CTLW0 &= ~UCTR;   /* Configure as receiver */
+    EUSCI_SEL->CTLW0 |= UCTXSTT; /* Send RESTART condition */
+    while (EUSCI_SEL->CTLW0 & UCTXSTT); /* Wait for start condition to be received */
+    if (EUSCI_SEL->IFG & EUSCI_B_IFG_NACKIFG) return false; /*If the slave does not acknowledge the transmitted data, the not-acknowledge interrupt flag UCNACKIFG is set*/
     for(uint16_t i = 0; i < data_len; ++i){
         if (i == data_len-1) {
-            USCI_REG_PTR(EUSCI_SEL,CTL1) |= UCTXSTP; /* Send stop before reading the last byte */
+            EUSCI_SEL->CTLW0 |= UCTXSTP; /* Send stop before reading the last byte */
         }
-        while (!(IFG2 & USCI_REG_VAR(EUSCI_SEL,RXIFG))); /*The UCRXIFG0 interrupt flag is set when a character is received and loaded into UCBxRXBUF*/
-        data[i] = USCI_REG_VAR(EUSCI_SEL,RXBUF); 
+        while (!(EUSCI_SEL->IFG & EUSCI_B_IFG_RXIFG0)); /*The UCRXIFG0 interrupt flag is set when a character is received and loaded into UCBxRXBUF*/
+        data[i] = EUSCI_SEL->RXBUF; 
     }
     return true;
 }
@@ -58,13 +58,13 @@ bool i2c_write_core(const uint16_t addr, uint8_t addr_len, const uint8_t *data, 
     if (!start_transfer(addr, addr_len)) return false; // start the transfer for adressing a slave's register
     
     for (uint16_t i = 0; i < data_len; ++i){
-        USCI_REG_VAR(EUSCI_SEL,TXBUF) = data[i]; /* write a byte in the buffer */
+        EUSCI_SEL->TXBUF = data[i]; /* write a byte in the buffer */
         /* CTXIFG2 is set when UCBxTXBUF is empty
         in slave mode, if the slave address defined in UCBxI2COA2 was on the bus in
         the same frame. */
-        while (!(IFG2 & USCI_REG_VAR(EUSCI_SEL,RXIFG))); 
+        while (!(EUSCI_SEL->IFG & EUSCI_B_IFG_RXIFG0)); 
         /*If the slave does not acknowledge the transmitted data, the not-acknowledge interrupt flag UCNACKIFG is set*/
-        if (USCI_REG_VAR(EUSCI_SEL,STAT) & UCNACKIFG) {
+        if (EUSCI_SEL->IFG & EUSCI_B_IFG_NACKIFG) {
             stop_transfer();
             return false;
         }
@@ -77,15 +77,22 @@ bool i2c_write_core(const uint16_t addr, uint8_t addr_len, const uint8_t *data, 
 void i2c_init()
 {
     // Primary function selection -> I2C
-    P6->SEL0 |= BIT5 + BIT4;
-    P6->SEL1 &= ~(BIT5 + BIT4);
+    I2C_SDA_PORT->SEL0 |= BIT_PIN(I2C_SDA_PIN);
+    I2C_SCL_PORT->SEL0 |= BIT_PIN(I2C_SCL_PIN);
+    I2C_SDA_PORT->SEL1 &= ~BIT_PIN(I2C_SDA_PIN);
+    I2C_SCL_PORT->SEL1 &= ~BIT_PIN(I2C_SCL_PIN);
 
-    USCI_REG_PTR(EUSCI_SEL,CTL1) |= UCSWRST; //eUSCI logic held in reset state (enable modifications)
-    USCI_REG_PTR(EUSCI_SEL,CTL0) = UCMST + UCSYNC + UCMODE_3;  //UCMST=1 sets Master mode, UCSYNC=1 sets Synchronous mode, UCMODE_3=1 sets I2C mode
-    USCI_REG_PTR(EUSCI_SEL,CTL1) |= UCSSEL_2; // sets clock source, UCSSEL_2 selects SMCLK
-    USCI_REG_VAR(EUSCI_SEL,BR0) = 10; //Bit Rate Control 1010
-    USCI_REG_VAR(EUSCI_SEL,BR1) = 0; //Bit Rate Control
-    USCI_REG_PTR(EUSCI_SEL,CTL1) &= ~UCSWRST;  //eUSCI reset released for operation
+    EUSCI_SEL->CTLW0 |= UCSWRST; //eUSCI logic held in reset state (enable modifications)
+    
+    // Changed '=' to '|=' here because MSP432 uses a 16-bit CTLW0 register. 
+    // '=' would overwrite and clear the UCSWRST bit we just set above.
+    EUSCI_SEL->CTLW0 |= UCMST + UCSYNC + UCMODE_3;  //UCMST=1 sets Master mode, UCSYNC=1 sets Synchronous mode, UCMODE_3=1 sets I2C mode
+    EUSCI_SEL->CTLW0 |= UCSSEL_2; // sets clock source, UCSSEL_2 selects SMCLK
+    
+    // MSP432 uses a single 16-bit BRW register instead of BR0/BR1
+    EUSCI_SEL->BRW = 10; //Bit Rate Control 1010
+    
+    EUSCI_SEL->CTLW0 &= ~UCSWRST;  //eUSCI reset released for operation
     i2c_set_slave_address(DEFAULT_SLAVE_ADDRESS); // set default slave address
 }
 
@@ -96,5 +103,5 @@ void i2c_set_slave_address(uint8_t addr)
 device to be addressed by the eUSCIx_B module. It is only used in master
 mode. The address is right justified. In 7-bit slave addressing mode, bit 6 is the
 MSB and bits 9-7 are ignored. In 10-bit slave addressing mode, bit 9 is the MSB.*/
-    USCI_REG_VAR(EUSCI_SEL,I2CSA) = addr; 
+    EUSCI_SEL->I2CSA = addr; 
 }
--- drivers/i2c.h
+++ drivers/i2c.h
@@ -7,12 +7,17 @@
 #include <stddef.h>
 
 #define DEFAULT_SLAVE_ADDRESS (0x29)
-#define EUSCI_SEL UCB1
 
-#define USCI_REG_VAR(usci,reg)     GLUE2_VAR(usci, reg) 
-#define GLUE2_VAR(a, b)       a ## b
+/* Updated to the MSP432 CMSIS peripheral pointer */
+#define EUSCI_SEL EUSCI_B1
+#define I2C_SCL_PORT 6
+#define I2C_SCL_PIN 5
+#define I2C_SDA_PORT 6
+#define I2C_SDA_PIN 4
 
-#define USCI_REG_PTR(usci,reg)     ((usci)->reg)
+#define BIT_PIN(pin) (BIT## pin)
+
+/* Removed the invalid USCI_REG_VAR, GLUE2_VAR, and USCI_REG_PTR macros */
 
 void i2c_init(void);
 
@@ -26,8 +31,7 @@ bool i2c_read_core(const uint16_t addr, uint8_t addr_len, uint8_t *data, uint16_
 
 bool i2c_write_core(const uint16_t addr, uint8_t addr_len, const uint8_t *data, uint16_t data_len);
 
-void i2c_init();
-
-void i2c_set_slave_address(uint8_t addr);
+/* Note: Removed the duplicate i2c_init and i2c_set_slave_address declarations 
+   from the end of this file to resolve the "#174-D linkage conflict" error */
 
 #endif /* I2C_H */
