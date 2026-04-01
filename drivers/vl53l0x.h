5327f920ba10d21bc2ad54adedf32457ba60795b
Biasio <alessandro.biasioli@gmail.com>
2026-04-01 17:46:38 +0200

    changed some macro expansion definitions

--- drivers/vl53l0x.h
+++ drivers/vl53l0x.h
@@ -11,13 +11,13 @@
 #define XSHUT_PIN 1
 
 // Pre-glue macros for correct argument expansion
-#define PORT_REG(port, reg) ((P##port)->(reg)) // ex: PORT_REG(2, SEL) -> P2SEL
+#define PORT_REG(port, reg) (GLUE2(P, port) -> (reg)) // ex: PORT_REG(2, SEL) -> P2SEL
 #define PIN_TO_BIT(pin)     GLUE2(BIT, pin)     // ex: PIN_TO_BIT(0)    -> BIT0
 
 
 // Glue macros (no expansion of the macros passed as arguments)
 #define GLUE2(a, b)       a ## b
-#define GLUE3(a, b, c)    a ## b -> c
+
 
 #define VL53L0X_OUT_OF_RANGE (8190)
 
