68913c08c791c95353f46cf4d445f010e24a8d9f
Biasio <alessandro.biasioli@gmail.com>
2026-05-05 16:57:12 +0200

    changed boot up timing

--- drivers/vl53l0x.c
+++ drivers/vl53l0x.c
@@ -700,8 +700,8 @@ static bool init_address()
 {
     xshut_toggle(true);
     i2c_set_slave_address(VL53L0X_DEFAULT_ADDRESS);
-    __delay_us(10000);
-    return WAIT_UNTIL(device_is_booted(), 2000);
+    __delay_us(4000);
+    return device_is_booted();
 }
 
 
