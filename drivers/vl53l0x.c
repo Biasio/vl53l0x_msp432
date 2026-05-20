5a6cd6eea3d395e45835e130b85dc0d81fdf6193
Biasio <alessandro.biasioli@gmail.com>
2026-05-20 16:41:53 +0200

    fixed always clear_int

--- drivers/vl53l0x.c
+++ drivers/vl53l0x.c
@@ -1158,7 +1158,7 @@ bool clear_interrupt(){
     do {
         // Clear interrupts
         if(!i2c_write(REG_SYSTEM_INTERRUPT_CLEAR, 1, (uint8_t[]){0x01}, 1)) return false;
-        //if(!i2c_write(REG_SYSTEM_INTERRUPT_CLEAR, 1, (uint8_t[]){0x00}, 1)) return false;
+        if(!i2c_write(REG_SYSTEM_INTERRUPT_CLEAR, 1, (uint8_t[]){0x00}, 1)) return false;
         // Read status to check if interrupts were cleared
         if (!i2c_read(REG_RESULT_INTERRUPT_STATUS, 1, &status, 1)) return false;
         if (!(--timeout)) return false;
